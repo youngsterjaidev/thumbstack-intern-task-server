d527151e68eec9bcbd27e10ec4cec182ba54dddf
Youngster Jaidev <jaidevv999@gmail.com>
2021-10-04 19:45:14 +0530

    Add the build

--- src/index.js
+++ src/index.js
@@ -7,18 +7,19 @@ const pdf = require("html-pdf");
 const path = require("path");
 const morgan = require("morgan");
 
-const app = require("express")();
+const express = require("express")
+const app = express()
 const port = process.env.PORT || 8080;
 const uri = process.env.URI;
 
 // Define font files
 var fonts = {
-  Roboto: {
-    normal: "fonts/Roboto-Regular.ttf",
-    bold: "fonts/Roboto-Medium.ttf",
-    italics: "fonts/Roboto-Italic.ttf",
-    bolditalics: "fonts/Roboto-MediumItalic.ttf"
-  }
+    Roboto: {
+        normal: "fonts/Roboto-Regular.ttf",
+        bold: "fonts/Roboto-Medium.ttf",
+        italics: "fonts/Roboto-Italic.ttf",
+        bolditalics: "fonts/Roboto-MediumItalic.ttf",
+    },
 };
 
 var PdfPrinter = require("pdfmake");
@@ -28,112 +29,134 @@ var printer = new PdfPrinter(fonts);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //app.use(morgan.dev());
+app.use(express.static("build"))
 app.use(cors());
 
 (async () => {
-  try {
-    let client = await MongoClient.connect(uri);
+    try {
+        let client = await MongoClient.connect(uri);
 
-    const col = client.db("thumbstack").collection("items");
+        const db = client.db("thumbstack");
 
-    app.get("/", async (req, res) => {
-      let result = await col.find({}).toArray();
-      console.log(result);
-      res.json({ message: "data", data: result });
-    });
+        app.get("/all", async (req, res) => {
+            let result = await db.collection("items").find({}).toArray();
+            console.log(result);
+            res.json({ message: "data", data: result });
+        });
 
-    app.get("/addItem", async (req, res) => {
-      let result = await col.insertMany([
-        {
-          itemName: "Cake",
-          quantity: 0,
-          price: 300
-        },
-        {
-          itemName: "Donut",
-          quantity: 0,
-          price: 200
-        },
-        {
-          itemName: "Pastries",
-          quantity: 0,
-          price: 150
-        }
-      ]);
+        app.post("/add", async (req, res) => {
+            const { user, result, totalPrice } = req.body;
 
-      res.json({ mesage: "Inserted", data: result });
-    });
+            try {
+                let feedback = await db.collection("user").updateOne(
+                    { user: user },
+                    {
+                        $set: {
+                            result: result,
+                            totalPrice: totalPrice,
+                        },
+                    },
+                    {
+                        upsert: true,
+                    }
+                );
 
-    app.post("/add", async (req, res) => {
-      console.log(req);
-      const { user, result, totalPrice } = req.body;
+                console.log(feedback)
 
-      let data = {
-        content: [
-          { text: `${user} - Rs.${totalPrice}`, style: "header" },
-          { text: "Jackinos", style: "subheader" },
-          {
-            style: "tableExample",
-            table: {
-              body: [[{ text: "Item Name" }, "Quantity", "Price (RS)"]]
+                res.status(200).json(feedback);
+            } catch (e) {
+                console.error("Error Occured : ", e);
             }
-          }
-        ],
-        styles: {
-          header: {
-            fontSize: 18,
-            bold: true,
-            margin: [0, 0, 0, 10]
-          },
-          subheader: {
-            fontSize: 16,
-            bold: true,
-            margin: [0, 10, 0, 5]
-          },
-          tableExample: {
-            margin: [0, 5, 0, 15]
-          },
-          tableHeader: {
-            bold: true,
-            fontSize: 13,
-            color: "black"
-          }
-        }
-      };
+        });
 
-      result.forEach(({ itemName, quantity, price }) => {
-        data.content[2].table.body.push([itemName, quantity, price]);
-      });
+        app.get("/user/:email", async (req, res) => {
+            const { email } = req.params;
 
-      console.log(data);
+            console.log(email)
 
-      let pdfDoc = printer.createPdfKitDocument(data, {});
+            let feedback = await db
+                .collection("user")
+                .findOne({
+                    user: email,
+                })
 
-      // pdfDoc.pipe(fs.createWriteStream("document.pdf"));
-      // pdfDoc.end();
+            console.log(feedback)
 
-      // var file = fs.createReadStream("./yarn.lock");
-      // var stat = fs.statSync("./yarn.lock");
-      // res.setHeader("Content-Length", stat.size);
-      res.setHeader("Content-Type", "application/pdf");
-      res.setHeader("Content-Disposition", "attachment; filename=input.pdf");
+            if (!feedback) return res.status(500).send("Internal Server Error");
 
-      // console.log(pdfDoc);
+            const { user, totalPrice, result } = feedback;
 
-      // // pipe the stream to response
-      pdfDoc.pipe(res);
-      pdfDoc.end();
+            let data = {
+                content: [
+                    { text: `${user} - Rs.${totalPrice}`, style: "header" },
+                    { text: "Jackinos", style: "subheader" },
+                    {
+                        style: "tableExample",
+                        table: {
+                            body: [
+                                [
+                                    { text: "Item Name" },
+                                    "Quantity",
+                                    "Price (RS)",
+                                ],
+                            ],
+                        },
+                    },
+                ],
+                styles: {
+                    header: { fontSize: 18, bold: true, margin: [0, 0, 0, 10] },
+                    subheader: {
+                        fontSize: 16,
+                        bold: true,
+                        margin: [0, 10, 0, 5],
+                    },
+                    tableExample: {
+                        margin: [0, 5, 0, 15],
+                    },
+                    tableHeader: {
+                        bold: true,
+                        fontSize: 13,
+                        color: "black",
+                    },
+                },
+            };
 
-      // res.download();
-      // file.pipe(res);
+            result.forEach(({ itemName, quantity, price }) => {
+                data.content[2].table.body.push([itemName, quantity, price]);
+            });
 
-      //res.send("Okay");
-    });
+            console.log(data);
 
-    app.listen(port, () => {
-      console.log(`Server is running at port ${port}`);
-    });
-  } catch (e) {
-    console.error(`Error Occured : `, e);
-  }
+            let pdfDoc = printer.createPdfKitDocument(data, {});
+
+            // pdfDoc.pipe(fs.createWriteStream("document.pdf"));
+            // pdfDoc.end();
+
+            // var file = fs.createReadStream("./yarn.lock");
+            // var stat = fs.statSync("./yarn.lock");
+            // res.setHeader("Content-Length", stat.size);
+            res.setHeader("Content-Type", "application/pdf");
+            res.setHeader(
+                "Content-Disposition",
+                "attachment; filename=input.pdf"
+            );
+
+            // console.log(pdfDoc);
+
+            // // pipe the stream to response
+            pdfDoc.pipe(res);
+            pdfDoc.end();
+
+            // res.download();
+            // file.pipe(res);
+
+            //res.send("Okay");
+        });
+
+        app.listen(port, () => {
+            console.log(`Server is running at port ${port}`);
+        });
+    } catch (e) {
+        console.error(`Error Occured : `, e);
+    }
 })();
